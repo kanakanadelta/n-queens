7b8be4bd330d6ff4e41a50ac69017e46c40ead77
kanakanadelta <kanakanadelta@gmail.com>
2018-08-09 20:07:07 -0700

    Completed sprint except for find single solution for Queens. Able to have array of arrays, but error occurs.

--- src/solvers.js
+++ src/solvers.js
@@ -47,7 +47,6 @@ window.countNRooksSolutions = function(n) {
   //iterating through the columns
   //function that takes row as an input and iterates through the rows recursively
   //  -->finds possible solutions in the possible solution branches
-  debugger;
   var iterateThroughRows = function(row) {
     if (row === n) {
       solutionCount++;
@@ -77,31 +76,76 @@ window.countNRooksSolutions = function(n) {
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = new Board({n:n}); //fixme
-  /*
-  var numPieces = _.reduce(solution.rows(), function(memo, row) {
-    return memo + _.reduce(row, function(memo, col) {
-      return memo + col;
-    }, 0);
-  }, 0);
-  //need to check if number of pieces on the board is equal to n
-  //if not must reset board and loop through with different column start
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      console.log('before: ', solution.rows());
-      solution.togglePiece(i, j);
-      if (solution.hasAnyQueensConflicts(i, j)) {
-        solution.togglePiece(i, j);
-        console.log('after: ', solution.rows());
+  var solutionArray;
+  // var numPieces = _.reduce(solution.rows(), function(memo, row) {
+  //   return memo + _.reduce(row, function(memo, col) {
+  //     return memo + col;
+  //   }, 0);
+  // }, 0);
+  // //need to check if number of pieces on the board is equal to n
+  // //if not must reset board and loop through with different column start
+
+  // for (let i = 0; i < n; i++) {
+  //   for (let j = 0; j < n; j++) {
+  //     console.log('before: ', solution.rows());
+  //     solution.togglePiece(i, j);
+  //     if (solution.hasAnyQueensConflicts()) {
+  //       console.log(numPieces)
+  //       solution.togglePiece(i, j);
+  //       console.log('after: ', solution.rows());
+  //     }
+  //   }
+  // }
+
+  // var solution = solution.rows();
+
+  // // console.log('queens: ' + solution);
+  var iterateThroughRows = function(row) {
+    if (row === n) {
+      // solution = new Board(solution);
+      // return solutionArray.rows();
+      // console.log(solution);
+      solutionArray = JSON.parse(JSON.stringify(solution));
+      return solutionArray;
+    }
+
+    for (let i = 0; i < n; i++) {
+      solution.togglePiece(row, i);
+
+      if (!solution.hasAnyQueensConflicts()) {
+        //iterate through the remainder of the board with pieces of current branch still in place 
+        // let solutionArray = _.map(solution, function(rowIndex) {
+        //   return rowIndex
+        // })
+
+        // for(var j = 0; j < solution.rows().length; j++){
+        //   solutionArray.push(solution.rows()[j]);
+        // }
+        // solutionArray.push(JSON.parse(JSON.stringify(solution.get(row))));
+        iterateThroughRows(row + 1);
+        //take current piece off and continue iterating through the columns
       }
+
+      solution.togglePiece(row, i);
     }
   }
 
-  var solution = solution.rows();
-  if ()
-  // console.log('queens: ' + solution);
-  */
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  //start from first row
+  iterateThroughRows(0);
+
+  solutionArray = Object.keys(solutionArray).map(function(key) {
+    return solutionArray[key];
+  });
+
+  solutionArray.pop();
+  // console.log(solutionArray);
+  // let noMoreBrackets = [];
+  // for (let i = 0; i < solutionArray.length; i++) {
+  //   noMoreBrackets.push(solutionArray[i][0]);
+  // }
+
+  console.log('Single solution for ' + n + ' queens:', solutionArray);
+  return solutionArray;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
@@ -112,7 +156,6 @@ window.countNQueensSolutions = function(n) {
   //iterating through the columns
   //function that takes row as an input and iterates through the rows recursively
   //  -->finds possible solutions in the possible solution branches
-  debugger;
   var iterateThroughRows = function(row) {
     if (row === n) {
       solutionCount++;
